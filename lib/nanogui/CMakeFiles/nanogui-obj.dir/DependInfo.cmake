a7ff6787d9442228413103386369f12943b8342e
Paul <pmiiller@sfu.ca>
2019-03-28 13:45:01 -0700

    lets see if this fixes the nanogui issue

---
+++ lib/nanogui/CMakeFiles/nanogui-obj.dir/DependInfo.cmake
@@ -0,0 +1,103 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/ext/nanovg/src/nanovg.c" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/ext/nanovg/src/nanovg.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CORO_SJLJ"
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_BUILD"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_BUILD"
+  "NVG_SHARED"
+  "_GLFW_BUILD_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "lib/nanogui/ext/eigen"
+  "lib/nanogui/ext/glfw/include"
+  "lib/nanogui/ext/nanovg/src"
+  "lib/nanogui/include"
+  "lib/nanogui"
+  "lib/nanogui/ext/coro"
+  "lib/nanogui/ext/pybind11/include"
+  "/usr/include/python3.5m"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/nanogui_resources.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/nanogui_resources.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/button.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/button.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/checkbox.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/checkbox.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/colorpicker.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/colorpicker.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/colorwheel.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/colorwheel.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/combobox.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/combobox.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/common.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/common.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/glcanvas.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/glcanvas.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/glutil.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/glutil.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/graph.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/graph.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/imagepanel.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/imagepanel.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/imageview.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/imageview.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/label.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/label.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/layout.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/layout.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/messagedialog.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/messagedialog.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/popup.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/popup.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/popupbutton.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/popupbutton.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/progressbar.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/progressbar.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/screen.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/screen.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/serializer.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/serializer.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/slider.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/slider.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/stackedwidget.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/stackedwidget.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/tabheader.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/tabheader.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/tabwidget.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/tabwidget.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/textbox.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/textbox.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/theme.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/theme.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/vscrollpanel.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/vscrollpanel.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/widget.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/widget.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/src/window.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-obj.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CORO_SJLJ"
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_BUILD"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_BUILD"
+  "NVG_SHARED"
+  "_GLFW_BUILD_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/nanogui/ext/eigen"
+  "lib/nanogui/ext/glfw/include"
+  "lib/nanogui/ext/nanovg/src"
+  "lib/nanogui/include"
+  "lib/nanogui"
+  "lib/nanogui/ext/coro"
+  "lib/nanogui/ext/pybind11/include"
+  "/usr/include/python3.5m"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/nanogui_resources.h" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/nanogui_resources.cpp"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/nanogui/CMakeFiles/nanogui-python-obj.dir/DependInfo.cmake
@@ -0,0 +1,77 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/ext/coro/coro.c" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/ext/coro/coro.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CORO_SJLJ"
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_SHARED"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "lib/nanogui/ext/eigen"
+  "lib/nanogui/ext/glfw/include"
+  "lib/nanogui/ext/nanovg/src"
+  "lib/nanogui/include"
+  "lib/nanogui"
+  "lib/nanogui/ext/coro"
+  "lib/nanogui/ext/pybind11/include"
+  "/usr/include/python3.5m"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/basics.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/basics.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/button.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/button.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/constants_entypo.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/constants_entypo.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/constants_glfw.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/constants_glfw.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/eigen.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/eigen.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/formhelper.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/formhelper.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/glcanvas.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/glcanvas.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/glutil.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/glutil.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/layout.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/layout.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/main.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/main.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/misc.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/misc.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/nanovg.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/nanovg.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/tabs.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/tabs.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/textbox.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/textbox.cpp.o"
+  "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/python/widget.cpp" "/home/wallace/Desktop/nano/nanogui-test/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/widget.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CORO_SJLJ"
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_SHARED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/nanogui/ext/eigen"
+  "lib/nanogui/ext/glfw/include"
+  "lib/nanogui/ext/nanovg/src"
+  "lib/nanogui/include"
+  "lib/nanogui"
+  "lib/nanogui/ext/coro"
+  "lib/nanogui/ext/pybind11/include"
+  "/usr/include/python3.5m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
